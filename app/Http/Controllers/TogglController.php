dcc2c0cba44150d25b392febc1a2de05d65be675
Zain <zainengineer@gmail.com>
2016-10-29 14:39:41 +1030

    display time

--- app/Http/Controllers/TogglController.php
+++ app/Http/Controllers/TogglController.php
@@ -6,10 +6,80 @@ use \App\Toggl;
 
 class TogglController extends Controller
 {
+    protected $iMondayToleranceHours = 72;
+
     public function test(Toggl\TimeEntries $oHelper, Request $oRequest)
     {
         $aTimeEntries = $oHelper->getEntriesByProject();
-        echo "<pre>";
-        print_r($aTimeEntries);
+//        echo "<pre>";
+//        print_r($aTimeEntries);
+        $this->displayTimeEntries($aTimeEntries);
+    }
+
+    protected function displayTimeEntries($aPersonInfo)
+    {
+        $aDayGrandTotal = [];
+        $fWeekGrandTotal = 0;
+        $vClosestMonday = $this->getClosestMonday();
+        $fClosestMonday = strtotime($vClosestMonday);
+        echo "<pre>\n";
+        ob_start();
+        foreach ($aPersonInfo as $vProjectName => $aProjectInfo) {
+            $bShowProject = true;
+            foreach ($aProjectInfo as $ticket => $aTicketEntries) {
+                $vTicket = is_numeric($ticket) ? $ticket : 'No Ticket';
+                $bShowTicket = true;
+                foreach ($aTicketEntries as $vDate => $aTimeEntries) {
+                    if (strtotime($vDate) < $fClosestMonday) {
+                        continue;
+                    }
+                    if ($bShowProject) {
+                        echo "$vProjectName\n";
+                        $bShowProject = false;
+
+                    }
+                    if ($bShowTicket) {
+                        echo "  $vTicket\n";
+                        $bShowTicket = false;
+                    }
+                    if (!isset($aDayGrandTotal[$vDate])) {
+                        $aDayGrandTotal[$vDate] = 0;
+                    }
+                    $fTicketTotal = 0;
+                    $fDuration = 0;
+                    echo "\n    $vDate\n";
+                    foreach ($aTimeEntries as $aSingleTimeEntry) {
+                        $fDuration = $aSingleTimeEntry['duration'];
+                        $fTicketTotal += $fDuration;
+                        $aDayGrandTotal[$vDate] += $fDuration;
+                        $fWeekGrandTotal += $fDuration;
+                        echo "      $fDuration  {$aSingleTimeEntry['description']} \n";
+                    }
+                    if (abs($fDuration - $fTicketTotal) > 0.0001) {
+                        echo "    $fTicketTotal\n";
+                    }
+                    echo "\n";
+                }
+                echo "\n";
+            }
+            echo "\n";
+        }
+        $vContents = ob_get_clean();
+        echo trim($vContents);
+        echo "\n\n";
+        foreach ($aDayGrandTotal as $vDate => $fDuration) {
+            echo "$vDate\t\t$fDuration\n";
+        }
+        echo "\nWeek GrandTotal\t\t" . $fWeekGrandTotal;
+    }
+
+    public function getClosestMonday()
+    {
+        $fLastMonday = strtotime('last Monday');
+        $fDiff = abs($fLastMonday - time()) / 60 / 60;
+        if ($fDiff < $this->iMondayToleranceHours) {
+            $fLastMonday = $fLastMonday - (7 * 24 * 60 * 60);
+        }
+        return date('Y-m-d', $fLastMonday);
     }
 }
--- app/Toggl/ApiHelper.php
+++ app/Toggl/ApiHelper.php
@@ -4,11 +4,13 @@ namespace App\Toggl;
 use Illuminate\Support\Facades\Cache;
 class ApiHelper
 {
+    protected $bEnableCache;
     /** @var static \AJT\Toggl\TogglClient */
     protected $oClient;
 
     public function __construct()
     {
+        $this->bEnableCache = empty($_GET['disable_cache']);
         $vTogglApiKey = $this->getKey();
         $this->oClient = \AJT\Toggl\TogglClient::factory(['api_key' => $vTogglApiKey]);
     }
@@ -18,7 +20,7 @@ class ApiHelper
 //        $expiresAt = \Carbon\Carbon::now()->addMinutes(10);
         $vCacheKey = 'time_entries_' . $this->getKey();
         $aTimeList = Cache::get($vCacheKey);
-        if (!$aTimeList){
+        if (!$aTimeList || !$this->bEnableCache){
             $aTimeList = $this->oClient->getTimeEntries(array());
             Cache::put($vCacheKey,$aTimeList,20);
         }
